4217fa9911bafdfe6f6751bead118b628f9104ad
UzhNaSkovorodke <piratt.kiri@gmail.com>
2022-03-14 14:51:02 +0700

    14 march

---
+++ NodeJs2/my-app/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Nav</header>
+      <main className="App-main">Apps</main>
+      <footer className="App-footer">Footer</footer>
+    </div>
+  );
+}
+
+export default App;
