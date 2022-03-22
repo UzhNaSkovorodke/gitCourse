e7c589e8ad247f2c95b30c57e1181a1897333eab
UzhNaSkovorodke <piratt.kiri@gmail.com>
2022-03-22 16:34:41 +0700

    22.03

--- NodeJs2/my-app/src/Pages/Main.js
+++ NodeJs2/my-app/src/Pages/Main.js
@@ -7,7 +7,7 @@ export const Main = () => {
       <div className="appAboutMe" id="appAboutMe">
         <div className="blockAbout">
           <h2 className="textAbout">About me</h2>
-          <img className="pngAvatar" src={avatarPng}></img>
+          <img className="pngAvatar" src={avatarPng} alt=""></img>
         </div>
         <div className="blockPhoto">
           <div className="containerPhoto">a</div>
@@ -15,7 +15,13 @@ export const Main = () => {
           <div className="containerPhoto">a</div>
         </div>
       </div>
-      <div className="appProjects" id="appProjects"></div>
+      <div className="appProjects" id="appProjects">
+        <div className="blockProjects">
+          <h2>lorem ipsum</h2>
+          <h3>lorem ipsum</h3>
+          <h3>lorem ipsum</h3>
+        </div>
+      </div>
       <div className="appContacts" id="appContacts"></div>
     </div>
   );
