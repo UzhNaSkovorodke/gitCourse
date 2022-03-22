a8364f713ae26620332119b2f128c427e5cb70c8
UzhNaSkovorodke <piratt.kiri@gmail.com>
2022-03-22 20:28:10 +0700

    it's sooo bad

--- NodeJs2/my-app/src/Pages/Header.js
+++ NodeJs2/my-app/src/Pages/Header.js
@@ -3,13 +3,13 @@ import React from "react";
 export const Header = () => {
   return (
     <div className="listNav">
-      <div className="navParagraph">
+      <div id="buttonHeader" className="navParagraph">
         <a href="#appHeader">About me</a>
       </div>
-      <div className="navParagraph">
+      <div id="buttonParagraph" className="navParagraph">
         <a href="#appProjects">Projects</a>
       </div>
-      <div className="navParagraph">
+      <div id="buttonContacts" className="navParagraph">
         <a href="#appContacts">Contacts</a>
       </div>
     </div>
