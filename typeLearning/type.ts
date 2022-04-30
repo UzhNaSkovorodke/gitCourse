867dbe22038c3b8c4194d51e86740a69179d9e0c
UzhNaSkovorodke <piratt.kiri@gmail.com>
2022-04-30 19:09:13 +0700

    typeScript

---
+++ typeLearning/type.ts
@@ -0,0 +1,23 @@
+let str: string = "Hello";
+console.log(str);
+
+let newValue1: boolean = true;
+console.log(newValue1);
+
+let newValue2: number = 2e3;
+console.log(newValue2);
+
+let newValue3: object = {
+  name: "Kirill",
+  age: 23,
+};
+console.log(newValue3);
+
+let newValue4: any = "OOooopps";
+console.log(newValue4);
+
+let newValue5: string[] = ["Kirill", "Jesus"];
+console.log(newValue5);
+
+let newValue6: [string, number, string] = ["kop", 2321, "Dsss"];
+console.log(newValue6);
